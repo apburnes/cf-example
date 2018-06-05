204facb69b49d09c2424f252c814d7cbcde3a56b
Andrew Burnes <andrew.burnes@gsa.gov>
2018-06-05 11:43:45 -0300

    update ci config

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send(`Hello ${process.env.BRANCH}!`));
+app.get('/', (req, res) => res.send(`Hello World!`));
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
