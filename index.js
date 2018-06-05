658af27a05707f611baed0073fbbcc8abb59797d
Andrew Burnes <andrew.burnes@gsa.gov>
2018-06-05 11:51:50 -0300

    Change message

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send(`Hello World!`));
+app.get('/', (req, res) => res.send(`Hello Develop!`));
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
